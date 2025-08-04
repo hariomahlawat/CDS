0b57b08e5178b8209234e4813eb8e42911188080
Hari Om Ahlawat <hariomahlawat@gmail.com>
2025-08-04 11:53:49 +0530

    experimental API

--- app/src/main/java/com/concepts_and_quizzes/cds/ui/dashboard/GlobalDashboardScreen.kt
+++ app/src/main/java/com/concepts_and_quizzes/cds/ui/dashboard/GlobalDashboardScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -16,6 +17,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GlobalDashboardScreen(
     navController: NavHostController,
