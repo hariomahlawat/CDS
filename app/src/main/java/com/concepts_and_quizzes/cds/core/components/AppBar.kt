432c40301ffbccce4b75af0c36a5365ce8e80298
Hari Om Ahlawat <hariomahlawat@gmail.com>
2025-08-04 11:45:30 +0530

    import issue fixed.

--- app/src/main/java/com/concepts_and_quizzes/cds/core/components/AppBar.kt
+++ app/src/main/java/com/concepts_and_quizzes/cds/core/components/AppBar.kt
@@ -1,9 +1,11 @@
 package com.concepts_and_quizzes.cds.core.components
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CdsAppBar(title: String) {
     TopAppBar(title = { Text(text = title) })
