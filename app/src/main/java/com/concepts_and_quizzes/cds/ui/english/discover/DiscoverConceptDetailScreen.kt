c4930721f5c42d7845bc0b66c733f7a57e3d061c
Hari Om Ahlawat <hariomahlawat@gmail.com>
2025-08-07 17:21:44 +0530

    imports / deprecated use of api or methods / syntax error --> fixed

--- app/src/main/java/com/concepts_and_quizzes/cds/ui/english/discover/DiscoverConceptDetailScreen.kt
+++ app/src/main/java/com/concepts_and_quizzes/cds/ui/english/discover/DiscoverConceptDetailScreen.kt
@@ -7,14 +7,16 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.verticalScroll
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Lightbulb
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.StarBorder
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconToggleButton
@@ -32,6 +34,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.concepts_and_quizzes.cds.core.theme.Dimens
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DiscoverConceptDetailScreen(nav: NavHostController, vm: DiscoverConceptViewModel = hiltViewModel()) {
     val concept by vm.concept.collectAsState()
